f99e3e2aab7b23b4fe372d64b270fc26e9f1ed6c
TCLife <hs_chung@kaist.ac.kr>
2023-07-06 00:16:16 +0900

    position_answer added

--- position_answer.py
+++ position_answer.py
@@ -29,17 +29,16 @@ from data import load_data
 if __name__ =='__main__':
     parser =argparse.ArgumentParser()
     parser.add_argument('--labels', type=int, default=1)
-    parser.add_argument('--batch_size', type=int, default=1)
-    parser.add_argument('--dataset', type =str,default='mitbih, flat, peak')
+    parser.add_argument('--batch_size', type=int, default=80)
+    parser.add_argument('--dataset', type =str,default='peak')
     parser.add_argument('--num_classes', type=int, default=2)
-    parser.add_argument('--vqvae_model', type=str, default ="/home/hschung/saved_models/timeseries/flat/model_360.pt") 
-    parser.add_argument('--model_type',type =str,default='cnn_transformer', help='cnn_transformer, transformer, cnn')
+    parser.add_argument('--vqvae_model', type=str, default ="/home/smjo/xai_timeseries/vqvae/saved_models/peak/2/model_280.pt") 
+    parser.add_argument('--model_type',type =str,default='cnn', help='cnn_transformer, transformer, cnn')
     parser.add_argument('--device', type =str,default='3')
     parser.add_argument('--auc_classification', type=bool, default=True)
-    parser.add_argument('--classification_model', type=str)
-    parser.add_argument('--mask_code', default=50)
-    parser.add_argument('--len_position', type=int, default=13)
-    parser.add_argument('--num_quantizers', type=int, default=[1,2,4,8])
+    parser.add_argument('--classification_model', type=str, default="/home/smjo/xai_timeseries/vqvae/saved_models/classification/peak/2/cnn.pt")
+    parser.add_argument('--len_position', type=int, default=12)
+    parser.add_argument('--num_quantizers', type=int, default=2)
     args = parser.parse_args()
     
     os.environ["CUDA_VISIBLE_DEVICES"] = args.device
@@ -61,74 +60,53 @@ if __name__ =='__main__':
     #Find masking token
     end_tokens={}
 
-    conv_net = VQ_Classifier(
+    net = VQ_Classifier(
         num_classes = 2,
         vqvae_model = vqvae_model,
         positions =0,
         mask = 0,
         auc_classification = False,
-        model_type = classifier
+        model_type = classifier,
+        len_position = len_position
     ).to(device)
 
     a = torch.load(classification_model)
-    conv_net.load_state_dict(a['model_state_dict'])
+    net.load_state_dict(a['model_state_dict'])
 
-    for param in conv_net.parameters():
+    for param in net.parameters():
         param.requires_grad = False
-
-    conv_net.eval()
         
-    masked_regions = conv_net.mask_region(ds.data[:64, :].to(device))
+    masked_regions = net.mask_region(ds.data[:64, :].to(device))
     
+    net = VQ_Classifier(
+    num_classes = 2,
+    vqvae_model = vqvae_model,
+    positions =0,
+    mask = masked_regions,
+    auc_classification = False,
+    model_type = classifier,
+    len_position = len_position
+    ).to(device)
     
-    #Make position answer
-    answer = {}         
-    arr = [i for i in range(len_position)]
-    roc_auc_scores=[]
+    a = torch.load(classification_model)
+    net.load_state_dict(a['model_state_dict'])
 
-
-    for cnt in range(len_position):
-        max_auc=-1
-        print("arr:", arr)
-        for i in arr:
-            #ECG Dataset
-            positions = list(answer.values())+[i]
-            net = VQ_Classifier(
-                num_classes = 2,
-                vqvae_model = vqvae_model,
-                positions = positions,
-                mask = masked_regions,
-                auc_classification = True,
-                model_type = classifier
-            ).to(device)
-            
-            
-            net.load_state_dict(torch.load(classification_model)["model_state_dict"])
-            net = net.to(device)
-            for param in net.parameters():
-                param.requires_grad = False        
-            net.eval()   
-
-            with torch.no_grad():    
-                score=0
-                for idx, (data, labels) in enumerate(test_loader):
-                    data = data.unsqueeze(1).float()
-                    labels = labels.type(torch.LongTensor)
-                    labels = torch.argmax(labels, dim=1)
-                    data, labels = data.to(device), labels.to(device)
-                    output, codebook_tokens, recon, input= net(data)
-                
-                    score += sklearn.metrics.roc_auc_score(labels.cpu(), output[:,1].cpu())
-                    
-            score = score / (idx+1)
-            if score > max_auc:
-                answer[f"top_{cnt}"]=i
-                max_auc = copy.deepcopy(score)
-        roc_auc_scores.append(max_auc)        
-        arr.remove(answer[f"top_{cnt}"])
-        
-    print(f"roc_auc_scores:{roc_auc_scores}")
-    print()
+    for param in net.parameters():
+        param.requires_grad = False
+    
+    net.eval()
+    selected_positions = []
+    
+    for i in range(len_position):
+        with torch.no_grad():    
+            for _, (data, labels) in enumerate(test_loader):
+                data = data.unsqueeze(1).float()
+                labels = labels.type(torch.LongTensor)
+                labels = torch.argmax(labels, dim=1)
+                data, labels = data.to(device), labels.to(device)
+                selected_positions = net.position_answer(data, i, labels, selected_positions)
+    
+    print(selected_positions)
                 
     
 
